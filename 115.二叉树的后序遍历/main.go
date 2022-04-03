974baa7412b74adabd8785887361a1df31f6b3dc
Ethan <slyfox_e@163.com>
2022-04-04 00:43:03 +0800

    二叉树的遍历

---
+++ 115.二叉树的后序遍历/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
+
+type arrayInt []int
+
+func (a *arrayInt) String() string {
+	return "my array int"
+}
+
+func (a *arrayInt) Set(value string) error {
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	*a = append(*a, i)
+	return nil
+}
+
+func main() {
+	var nums arrayInt
+	flag.Var(&nums, "l", "")
+	flag.Parse()
+    fmt.Printf("array : %v\n", nums)
+}
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+
+func inorderTraversal(root *TreeNode) []int {
+    res := []int{}
+
+    var inorder func(root *TreeNode)
+    inorder = func(root *TreeNode){
+        if root == nil {
+            return 
+        }
+        inorder(root.Left)
+        inorder(root.Right)
+        res = append(res, root.Val)
+    }
+
+    inorder(root)
+
+    return res
+}
